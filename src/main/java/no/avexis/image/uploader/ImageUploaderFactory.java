05cb63c4dd1970d12f6d2aaeb8afaf8b4a36c65b
Alexander Castillo <alexander_castillo@bitbucket.org>
2017-07-16 10:58:28 +0200

    Validate and create directory specified
    Check directory when creating builder, and try to create directory if specified

--- src/main/java/no/avexis/image/uploader/ImageUploaderFactory.java
+++ src/main/java/no/avexis/image/uploader/ImageUploaderFactory.java
@@ -3,8 +3,10 @@ package no.avexis.image.uploader;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.validation.ValidationMethod;
+import no.avexis.image.uploader.exceptions.ImageUploaderDirectoryMissingException;
 import org.hibernate.validator.constraints.NotEmpty;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +15,8 @@ public class ImageUploaderFactory {
     @JsonProperty
     @NotEmpty
     private String directory;
+    @JsonProperty
+    private boolean createDirectory;
     @JsonProperty
     private String filenameFormat;
     @JsonProperty
@@ -58,6 +62,21 @@ public class ImageUploaderFactory {
     }
 
     public ImageBuilder createBuilder() {
+        initDirectory();
         return new ImageBuilder(directory, filenameFormat, forcedFileType, allowedFileTypes, resolutions);
+    }
+
+    private void initDirectory() {
+        final File dir = new File(directory);
+        if (!dir.exists() && !createDirectory) {
+            throw new ImageUploaderDirectoryMissingException(String.format("Directory '%1$s' does not exist", directory));
+        } else if (!dir.isDirectory()) {
+            throw new ImageUploaderDirectoryMissingException(String.format("'%1$s' is not a directory", directory));
+        } else if (!dir.canWrite()) {
+            throw new ImageUploaderDirectoryMissingException(String.format("Can not write to directory '%1$s', insufficient permissions?", directory));
+        }
+        if (!dir.mkdirs()) {
+            throw new ImageUploaderDirectoryMissingException(String.format("Could not create directory '%1$s'", directory));
+        }
     }
 }
