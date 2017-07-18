11e0dcd6a8eb3f37cf439699a9e7e57388282ddd
Alexander Castillo <alexander_castillo@bitbucket.org>
2017-07-18 21:18:22 +0200

    Add private constructor

--- src/main/java/no/avexis/image/uploader/utils/ImageSaver.java
+++ src/main/java/no/avexis/image/uploader/utils/ImageSaver.java
@@ -6,10 +6,14 @@ import java.io.File;
 import java.io.IOException;
 
 public class ImageSaver {
+    private ImageSaver() {
+        throw new IllegalStateException("Utility class");
+    }
 
     public static boolean save(final String directory, final BufferedImage bufferedImage, final String filename) throws IOException {
         return save(directory, bufferedImage, filename);
     }
+
     public static boolean save(final String directory, final BufferedImage bufferedImage, final String filename, final String extension) throws IOException {
         final File imageDirectory = new File(directory);
         if (!imageDirectory.exists() && !imageDirectory.mkdirs()) {
