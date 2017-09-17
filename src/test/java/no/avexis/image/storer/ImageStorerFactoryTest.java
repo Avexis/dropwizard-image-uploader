48ed338e5ec4483b5b052997b268650e5302448b
Alexander Castillo <alexander_castillo@bitbucket.org>
2017-09-17 19:40:58 +0200

    Add test for createBuilder

---
+++ src/test/java/no/avexis/image/storer/ImageStorerFactoryTest.java
@@ -0,0 +1,53 @@
+package no.avexis.image.storer;
+
+import no.avexis.image.storer.models.ResolutionTemplate;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.powermock.api.mockito.PowerMockito.doNothing;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ImageStorerFactory.class})
+public class ImageStorerFactoryTest {
+
+    @Test
+    public void createBuilder() throws Exception {
+        final String directory = "directory";
+        final String filenameFormat = "filenameFormat";
+        final List<ResolutionTemplate> templates = new ArrayList<>();
+        final boolean runParallel = false;
+        final ImageStorerFactory isfSpy = PowerMockito.spy(new ImageStorerFactory(directory, false, filenameFormat, templates, runParallel));
+        doNothing().when(isfSpy, "initDirectory");
+
+        final ImageStorer imageStorer = isfSpy.createBuilder();
+
+        final Field directoryField = imageStorer.getClass().getDeclaredField("directory");
+        directoryField.setAccessible(true);
+        assertEquals("directory is passed on", directory, directoryField.get(imageStorer));
+
+
+        Field filenameFormatField = imageStorer.getClass().getDeclaredField("filenameFormat");
+        filenameFormatField.setAccessible(true);
+        assertEquals("filenameFormat is passed on", filenameFormat, filenameFormatField.get(imageStorer));
+
+
+        Field templatesField = imageStorer.getClass().getDeclaredField("templates");
+        templatesField.setAccessible(true);
+        assertEquals("templates is passed on", templates, templatesField.get(imageStorer));
+
+        Field runParallelField = imageStorer.getClass().getDeclaredField("runParallel");
+        runParallelField.setAccessible(true);
+        assertEquals("runParallel is passed on", runParallel, runParallelField.get(imageStorer));
+    }
+
+    public void addImageTransformer() {
+    }
+}
