0c4d4ad6224d44a9e27e5ac27f3ca26e3469f9c8
Alexander Castillo <alexander_castillo@bitbucket.org>
2017-09-17 19:09:40 +0200

    Switch to JUnit 4

--- src/test/java/no/avexis/image/storer/ImageStorerTest.java
+++ src/test/java/no/avexis/image/storer/ImageStorerTest.java
@@ -3,22 +3,20 @@ package no.avexis.image.storer;
 import no.avexis.image.storer.models.Resolution;
 import no.avexis.image.storer.transformers.AbstractImageTransformer;
 import no.avexis.image.storer.transformers.BasicImageTransformer;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+
 
 public class ImageStorerTest {
 
     @Test
-    @DisplayName("Get Transformer Exists")
-    void getTransformer_TransformerExists_OK() throws Exception {
+    public void getTransformer_TransformerExists_OK() throws Exception {
         final String key = "PNG";
 
         final Map<String, AbstractImageTransformer> transformers = new HashMap<>();
@@ -31,8 +29,7 @@ public class ImageStorerTest {
     }
 
     @Test
-    @DisplayName("Get Transformer Does Not Exists")
-    void getTransformer_TransformerDoesNotExist_Nullpointer() throws Exception {
+    public void getTransformer_TransformerDoesNotExist_Nullpointer() throws Exception {
         final String key = "JPG";
         final String exceptedMessage = "No ImageTransformer for fileType " + key;
 
@@ -41,52 +38,48 @@ public class ImageStorerTest {
 
         Method method = is.getClass().getDeclaredMethod("getTransformer", String.class);
         method.setAccessible(true);
-        final Throwable exception = assertThrows(InvocationTargetException.class, () -> method.invoke(is, key));
-        final Throwable innerException = exception.getCause();
-
-        assertEquals(NullPointerException.class, innerException.getClass());
-        assertEquals(exceptedMessage, innerException.getMessage());
+        try {
+            method.invoke(is, key);
+            fail();
+        } catch (final InvocationTargetException e) {
+            final Throwable innerException = e.getCause();
+            assertEquals(NullPointerException.class, innerException.getClass());
+            assertEquals(exceptedMessage, innerException.getMessage());
+        }
     }
 
-    @Nested
-    @DisplayName("Create Filename")
-    class CreateFilename {
-        @Test
-        @DisplayName("Case 1")
-        void CreateFilename_Case1() throws Exception {
-            final String filenameFormat = "%1$s_%4$s_%2$s_%3$s.%5$s";
-            final String filename = "somefile.jpg";
-            final Resolution resolution = new Resolution(60, 30, false);
-            final String templateName = "large";
+    @Test
+    public void CreateFilename_Case1() throws Exception {
+        final String filenameFormat = "%1$s_%4$s_%2$s_%3$s.%5$s";
+        final String filename = "somefile.jpg";
+        final Resolution resolution = new Resolution(60, 30, false);
+        final String templateName = "large";
 
-            final String expected = "somefile_large_60_30.jpg";
+        final String expected = "somefile_large_60_30.jpg";
 
-            final ImageStorer is = new ImageStorer("", filenameFormat, null, null, false);
-            Method method = is.getClass().getDeclaredMethod("createFilename", Resolution.class, String.class, String.class);
-            method.setAccessible(true);
-            final String actual = (String) method.invoke(is, resolution, templateName, filename);
+        final ImageStorer is = new ImageStorer("", filenameFormat, null, null, false);
+        Method method = is.getClass().getDeclaredMethod("createFilename", Resolution.class, String.class, String.class);
+        method.setAccessible(true);
+        final String actual = (String) method.invoke(is, resolution, templateName, filename);
 
-            assertEquals(expected, actual);
-        }
+        assertEquals(expected, actual);
+    }
 
-        @Test
-        @DisplayName("Case 2")
-        void CreateFilename_Case2() throws Exception {
-            final String filenameFormat = "w%2$s_h%3$s_%4$s_%1$s.%5$s";
-            final String filename = "someotherfile.png";
-            final Resolution resolution = new Resolution(100, 200, false);
-            final String templateName = "med";
+    @Test
+    public void CreateFilename_Case2() throws Exception {
+        final String filenameFormat = "w%2$s_h%3$s_%4$s_%1$s.%5$s";
+        final String filename = "someotherfile.png";
+        final Resolution resolution = new Resolution(100, 200, false);
+        final String templateName = "med";
 
-            final String expected = "w100_h200_med_someotherfile.png";
+        final String expected = "w100_h200_med_someotherfile.png";
 
-            final ImageStorer is = new ImageStorer("", filenameFormat, null, null, false);
-            Method method = is.getClass().getDeclaredMethod("createFilename", Resolution.class, String.class, String.class);
-            method.setAccessible(true);
-            final String actual = (String) method.invoke(is, resolution, templateName, filename);
+        final ImageStorer is = new ImageStorer("", filenameFormat, null, null, false);
+        Method method = is.getClass().getDeclaredMethod("createFilename", Resolution.class, String.class, String.class);
+        method.setAccessible(true);
+        final String actual = (String) method.invoke(is, resolution, templateName, filename);
 
-            assertEquals(expected, actual);
-        }
-
+        assertEquals(expected, actual);
     }
 
 }
--- src/test/java/no/avexis/image/storer/transformers/BasicImageTransformerTest.java
+++ src/test/java/no/avexis/image/storer/transformers/BasicImageTransformerTest.java
@@ -2,24 +2,24 @@ package no.avexis.image.storer.transformers;
 
 import no.avexis.image.storer.exceptions.ImageStorerException;
 import no.avexis.image.storer.models.ResolutionTemplate;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.net.URL;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
-class BasicImageTransformerTest {
+
+public class BasicImageTransformerTest {
 
     private static final String IMAGE_1 = "image_1_1920_1200.jpg";
     private BasicImageTransformer basicImageTransformer;
     private BufferedImage bufferedImage;
 
-    @BeforeEach
-    void buildBufferedImage() throws Exception {
+    @Before
+    public void buildBufferedImage() throws Exception {
         ClassLoader classLoader = getClass().getClassLoader();
         URL fileUrl = classLoader.getResource(IMAGE_1);
         bufferedImage = ImageIO.read(fileUrl);
@@ -27,8 +27,7 @@ class BasicImageTransformerTest {
     }
 
     @Test
-    @DisplayName("TransformBufferdImage")
-    void toBufferedImage_TransformByWidth() throws ImageStorerException {
+    public void toBufferedImage_TransformByWidth() throws ImageStorerException {
         final int expectedWidth = 192;
         final int expectedHeight = 120;
         final ResolutionTemplate template = new ResolutionTemplate(IMAGE_1, 192, 192, false, false);
@@ -40,8 +39,7 @@ class BasicImageTransformerTest {
 
 
     @Test
-    @DisplayName("TransformBufferdImage")
-    void toBufferedImage_TransformByHeight() throws ImageStorerException {
+    public void toBufferedImage_TransformByHeight() throws ImageStorerException {
         final int expectedWidth = 64;
         final int expectedHeight = 40;
         final ResolutionTemplate template = new ResolutionTemplate(IMAGE_1, 100, 40, false, false);
@@ -52,8 +50,7 @@ class BasicImageTransformerTest {
     }
 
     @Test
-    @DisplayName("TransformAndCropBufferedImage")
-    void toBufferedImage_transformCrop() throws ImageStorerException {
+    public void toBufferedImage_transformCrop() throws ImageStorerException {
         final int expectedWidth = 50;
         final int expectedHeight = 50;
         final ResolutionTemplate template = new ResolutionTemplate(IMAGE_1, 50, 50, true, false);
@@ -64,7 +61,7 @@ class BasicImageTransformerTest {
     }
 
     @Test
-    void toBase64() throws ImageStorerException {
+    public void toBase64() throws ImageStorerException {
         final int expectedWidth = 5;
         final int expectedHeight = 5;
         final String expectedString = "data:image/jpg;base64,/9j/4AAQSkZJRgABAgAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/2wBDAQkJCQwLDBgNDRgyIRwhMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjL/wAARCAAFAAUDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwC34lvjNcW8rR4Zo+drlRxjsOKKKKpIg//Z";
@@ -76,5 +73,4 @@ class BasicImageTransformerTest {
         final String base64String = basicImageTransformer.toBase64(bufferedImage, "jpg");
         assertEquals(expectedString, base64String);
     }
-
 }
