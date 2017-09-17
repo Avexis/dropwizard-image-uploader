018e3aa8b2ed55327bef8f3ecfb40e1f89c1a38e
Alexander Castillo <alexander_castillo@bitbucket.org>
2017-09-17 17:17:50 +0200

    Add ImageStorer Tests for getting transformers

--- src/test/java/no/avexis/image/storer/ImageStorerTest.java
+++ src/test/java/no/avexis/image/storer/ImageStorerTest.java
@@ -1,33 +1,92 @@
 package no.avexis.image.storer;
 
 import no.avexis.image.storer.models.Resolution;
+import no.avexis.image.storer.transformers.AbstractImageTransformer;
+import no.avexis.image.storer.transformers.BasicImageTransformer;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ImageStorerTest {
 
     @Test
-    @DisplayName("CreateFilename_FormatIsCorrectlyInserted")
-    void createFilename_FormatIsCorrectlyInserted() throws Exception {
-        final String filenameFormat = "%1$s_%4$s_%2$s_%3$s.%5$s";
-        final String filename = "somefile.jpg";
-        final Resolution resolution = new Resolution();
-        resolution.setWidth(60);
-        resolution.setHeight(30);
-        final String templateName = "large";
+    @DisplayName("Get Transformer Exists")
+    void getTransformer_TransformerExists_OK() throws Exception {
+        final String key = "PNG";
 
-        final String expected = "somefile_large_60_30.jpg";
+        final Map<String, AbstractImageTransformer> transformers = new HashMap<>();
+        transformers.put(key, new BasicImageTransformer());
+        final ImageStorer is = new ImageStorer("", "", transformers, null, false);
 
-        final ImageStorer is = new ImageStorer("", filenameFormat, null, null, false);
-        Method method = is.getClass().getDeclaredMethod("createFilename", Resolution.class, String.class, String.class);
+        Method method = is.getClass().getDeclaredMethod("getTransformer", String.class);
         method.setAccessible(true);
-        final String actual = (String) method.invoke(is, resolution, templateName, filename);
+        assertNotNull(method.invoke(is, key));
+    }
 
-        assertEquals(expected, actual);
+    @Test
+    @DisplayName("Get Transformer Does Not Exists")
+    void getTransformer_TransformerDoesNotExist_Nullpointer() throws Exception {
+        final String key = "JPG";
+        final String exceptedMessage = "No ImageTransformer for fileType " + key;
+
+        final Map<String, AbstractImageTransformer> transformers = new HashMap<>();
+        final ImageStorer is = new ImageStorer("", "", transformers, null, false);
+
+        Method method = is.getClass().getDeclaredMethod("getTransformer", String.class);
+        method.setAccessible(true);
+        final Throwable exception = assertThrows(InvocationTargetException.class, () -> method.invoke(is, key));
+        final Throwable innerException = exception.getCause();
+
+        assertEquals(NullPointerException.class, innerException.getClass());
+        assertEquals(exceptedMessage, innerException.getMessage());
+    }
+
+    @Nested
+    @DisplayName("Create Filename")
+    class CreateFilename {
+        @Test
+        @DisplayName("Case 1")
+        void CreateFilename_Case1() throws Exception {
+            final String filenameFormat = "%1$s_%4$s_%2$s_%3$s.%5$s";
+            final String filename = "somefile.jpg";
+            final Resolution resolution = new Resolution(60, 30, false);
+            final String templateName = "large";
+
+            final String expected = "somefile_large_60_30.jpg";
+
+            final ImageStorer is = new ImageStorer("", filenameFormat, null, null, false);
+            Method method = is.getClass().getDeclaredMethod("createFilename", Resolution.class, String.class, String.class);
+            method.setAccessible(true);
+            final String actual = (String) method.invoke(is, resolution, templateName, filename);
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        @DisplayName("Case 2")
+        void CreateFilename_Case2() throws Exception {
+            final String filenameFormat = "w%2$s_h%3$s_%4$s_%1$s.%5$s";
+            final String filename = "someotherfile.png";
+            final Resolution resolution = new Resolution(100, 200, false);
+            final String templateName = "med";
+
+            final String expected = "w100_h200_med_someotherfile.png";
+
+            final ImageStorer is = new ImageStorer("", filenameFormat, null, null, false);
+            Method method = is.getClass().getDeclaredMethod("createFilename", Resolution.class, String.class, String.class);
+            method.setAccessible(true);
+            final String actual = (String) method.invoke(is, resolution, templateName, filename);
+
+            assertEquals(expected, actual);
+        }
+
     }
 
 }
